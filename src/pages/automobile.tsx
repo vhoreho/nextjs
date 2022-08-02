c893a1440cf59ad3e5ad74dc386bbdbd83fd8459
vhoreho <vpubuntu@gmail.com>
2022-08-03 02:11:12 +0300

    feat: add content to pages

--- src/pages/automobile.tsx
+++ src/pages/automobile.tsx
@@ -1,26 +1,24 @@
+import {ReactElement} from "react";
 import useSWR from 'swr';
-import {v4} from "uuid";
-import {useRouter} from "next/router";
 import {fetcher} from "../utils/fetcher";
+import {Layout} from "../components/layout/Layout";
+import {News} from "../components/news/News";
+import {useRouter} from "next/router";
 
-function automobile() {
+export default function Automobile():ReactElement {
     const router = useRouter();
     const {data, error} = useSWR('automobiles', fetcher);
-
-    if(error) return "Something went wrong";
-    if(!data) return 'Loading...';
+    const section = router.pathname.slice(1);
 
     return (
-        <div>
-            <h2>Top Automobiles Stories:</h2>
-            <ul>
-                {data?.results.map(
-                    story =>
-                        <li key={v4()}>{story.title}</li>
-                )}
-            </ul>
-        </div>
+        <News data={data} error={error} section={section} />
     )
 }
 
-export default automobile;
+Automobile.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <Layout>
+            {page}
+        </Layout>
+    )
+}
