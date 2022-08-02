492bdee88843ec26561dd361a90f6bb94843598d
vhoreho <vpubuntu@gmail.com>
2022-08-03 02:07:31 +0300

    refactor: change variables.scss

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  images: {
+    domains: ['static01.nyt.com']
+  }
 }
 
 module.exports = nextConfig
---
+++ src/components/news/story/Story.tsx
@@ -0,0 +1,28 @@
+import styles from './Story.module.scss';
+import Image from "next/image";
+import moment from "moment";
+
+export const Story = ({story, section}) => {
+    const date = moment(story.published_date).format('LLL');
+    return (<li className={styles.card}>
+        <div className={styles.card__text}>
+            <span className={styles.card__section}>{!story.section ? section : story.section}</span>
+            <h2 className={styles.card__title}>{!story.title ? 'Title is not defined' : story.title}</h2>
+            <p className={styles.card__description}>{!story.abstract ? 'Title is not defined' : story.abstract}</p>
+            <span className={styles.card__date}>{date}</span>
+        </div>
+        <div className={styles.card__image}>
+            {
+                story.multimedia ? <Image
+                    src={story.multimedia[1].url}
+                    width={story.multimedia[1].width}
+                    height={story.multimedia[1].height}
+                    alt={story.title}
+                    layout='fixed'
+                    objectFit='contain'
+                /> : <h4>Image not found</h4>
+            }
+
+        </div>
+    </li>)
+}
