ad6aa803aa6ec671ed55e113889a1b3d6345fcf7
vhoreho <vpubuntu@gmail.com>
2022-08-03 02:07:31 +0300

    feat: add news, story components

---
+++ src/components/news/News.tsx
@@ -0,0 +1,24 @@
+import {FC} from "react";
+import {v4} from "uuid";
+import {Story} from "./story/Story";
+import styles from './News.module.scss';
+
+type NewsProps = {
+    data: any;
+    error: string;
+    section: string;
+}
+
+export const News:FC<NewsProps> = ({data,error,section}) => {
+    return (
+        <div className={styles.news}>
+            {error && <h2>Something went wrong</h2>}
+            {!data && <h2>Loading...</h2>}
+            <ul className={styles.list}>
+                {data?.results.map(
+                    story => <Story key={v4()} story={story} section={section} />
+                )}
+            </ul>
+        </div>
+    )
+}
