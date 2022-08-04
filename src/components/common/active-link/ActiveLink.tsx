e56f549ce36c61ff5c2866535a55b4b482ae7481
vhoreho <vpubuntu@gmail.com>
2022-08-04 10:37:35 +0300

    refactor: change active link type props

--- src/components/common/active-link/ActiveLink.tsx
+++ src/components/common/active-link/ActiveLink.tsx
@@ -2,12 +2,12 @@ import React, {useState, useEffect, ReactElement, Children, FunctionComponent} f
 import { useRouter } from 'next/router'
 import Link, { LinkProps } from 'next/link'
 
-type ActiveLinkProps = LinkProps & {
+type Props = LinkProps & {
     children: ReactElement
     activeClassName: string
 }
 
-export const ActiveLink:FunctionComponent<ActiveLinkProps> = ({
+export const ActiveLink:FunctionComponent<Props> = ({
                         children,
                         activeClassName,
                         ...props
@@ -19,16 +19,12 @@ export const ActiveLink:FunctionComponent<ActiveLinkProps> = ({
     const [className, setClassName] = useState(childClassName)
 
     useEffect(() => {
-        // Check if the router fields are updated client-side
         if (isReady) {
-            // Dynamic route will be matched via props.as
-            // Static route will be matched via props.href
             const linkPathname = new URL(
                 (props.as || props.href) as string,
                 location.href
             ).pathname
 
-            // Using URL().pathname to get rid of query and hash
             const activePathname = new URL(asPath, location.href).pathname
 
             const newClassName =
