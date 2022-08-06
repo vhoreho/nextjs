1aadf1ff037a9fa76ebc7f56aea4cd2f48e02cf5
vhoreho <vpubuntu@gmail.com>
2022-08-06 08:39:46 +0300

    refactor: add destructuring in news component

--- src/components/news/card/Card.tsx
+++ src/components/news/card/Card.tsx
@@ -4,18 +4,20 @@ import styles from './styles.module.scss';
 
 export const Card = ({story, section}) => {
   const date = moment(story.published_date).format('LLL');
+  const {section:category, title, abstract, multimedia} = story;
+
   return (<li className={styles.card}>
     <div className={styles.cardText}>
-      <span className={styles.cardSection}>{!story.section ? section : story.section}</span>
-      <h2 className={styles.cardTitle}>{!story.title ? 'Title is not defined' : story.title}</h2>
-      <p className={styles.cardDescription}>{!story.abstract ? 'Title is not defined' : story.abstract}</p>
+      <span className={styles.cardSection}>{!category ? section : category}</span>
+      <h2 className={styles.cardTitle}>{!title ? 'Title is not defined' : title}</h2>
+      <p className={styles.cardDescription}>{!abstract ? 'Title is not defined' : abstract}</p>
       <span className={styles.cardDate}>{date}</span>
     </div>
     <div className={styles.cardImage}>
       {
-        story.multimedia ? <Image
-          src={story.multimedia[1].url}
-          alt={story.title}
+        multimedia ? <Image
+          src={multimedia[1].url}
+          alt={title}
           layout='fill'
           objectFit='cover'
         /> : <h4>Image not found</h4>
