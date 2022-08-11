8f57955734f52ba06d442643a2e951baaf60de02
vhoreho <vpubuntu@gmail.com>
2022-08-11 18:25:07 +0300

    fix: fix fetcher and news component

--- src/services/article/fetcher.ts
+++ src/services/article/fetcher.ts
@@ -2,7 +2,6 @@ import { ArticleData } from "./types";
 
 export async function articleFetcher(url: string): Promise<ArticleData> {
   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/search/v2/articlesearch.json?fq=uri:("nyt:/${url}")&api-key=${process.env.NEXT_PUBLIC_KEY}`);
-  console.log('RUN FETCHER', url);
 
   try {
     return await response.json();
