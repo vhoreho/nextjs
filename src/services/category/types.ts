961ff03e029ce75678b496846abdc9302d173083
vhoreho <vpubuntu@gmail.com>
2022-08-12 12:52:25 +0300

    feat: add models

--- src/services/category/types.ts
+++ src/services/category/types.ts
@@ -1,36 +1,5 @@
-type Multimedia = {
-  caption: string;
-  copyright: string;
-  format: string;
-  height: number;
-  subtype: string;
-  type: string;
-  url: string;
-  width: number;
-}
-
-type FieldType = {
-  abstract?: string;
-  byline?: string;
-  created_date?: string;
-  des_facet?: string[];
-  geo_facet?: string[];
-  item_type?: string;
-  kicker?: string;
-  material_type_facet?: string;
-  multimedia?: Multimedia[];
-  org_facet?: string[];
-  per_facet?: string[];
-  published_date?: string;
-  section?: string;
-  short_url?: string;
-  subsection?: string;
-  title?: string;
-  updated_date?: string;
-  uri?: string;
-  url?: string;
-}
+import { SectionModel } from "models/section"
 
 export type SectionData = {
-  results: FieldType[]
+  results: SectionModel[];
 }
